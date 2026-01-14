8321a4e4ab0a2078497ac25b814e0cfa16a70a64
SauceCode01 <sauce.code.01@gmail.com>
2026-01-14 14:36:04 +0800

    env: fixed env config of web

--- apps/web/src/configs/servers.config.ts
+++ apps/web/src/configs/servers.config.ts
@@ -1,6 +1,4 @@
 export const configs = {
-  nexusApiBaseUrl:
-    process.env.NEXT_PUBLIC_NEXUS_API_URL || "http://localhost:8000/",
-  identityApiBaseUrl:
-    process.env.NEXT_PUBLIC_IDENTITY_API_URL || "http://localhost:8100/",
+  apiBaseUrl:
+    process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000/"
 };
